90fd023bf0b753da1756441864b32192be54b93e
manjushavnair <manjushavnair@gmail.com>
2017-02-01 02:30:18 -0500

    chg

--- TealeafSaaS/src/com/ibm/tealeaf/testcases/VerifyTealeafSaaSlogin.java
+++ TealeafSaaS/src/com/ibm/tealeaf/testcases/VerifyTealeafSaaSlogin.java
@@ -73,7 +73,7 @@ public class VerifyTealeafSaaSlogin {
 
 	}
 
-	/*@Test(priority=3)
+	@Test(priority=3)
 	public void struggleAnalytics() {
 		try {
 
@@ -97,7 +97,7 @@ public class VerifyTealeafSaaSlogin {
 
 		}	
 
-	}*/
+	}
 
 	@AfterSuite
 	public void testDown() {
