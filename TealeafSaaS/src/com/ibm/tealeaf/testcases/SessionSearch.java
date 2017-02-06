d48eb6893b12a936fe88303c7f54bf5a6321e897
manjushasaju <manjushavnair@gmail.com>
2017-02-06 19:57:15 +0530

    chg

--- TealeafSaaS/src/com/ibm/tealeaf/testcases/SessionSearch.java
+++ TealeafSaaS/src/com/ibm/tealeaf/testcases/SessionSearch.java
@@ -23,33 +23,27 @@ import com.ibm.tealeaf.pageobjects.SessionSearchPage;
 
 public class SessionSearch extends SaaSBasePage {
 
-	 
-	private static Logger logger = Logger
-			.getLogger(SessionSearch.class);
-
-	 
+	private static Logger logger = Logger.getLogger(SessionSearch.class);
 
 	public SessionSearch() {
 
-		//super();
+		// super();
 
 	}
 
-	 
-	@Test(priority = 1)
-	public void sessionSearch() {
+	@Test(priority = 2)
+	public void sessionSearchLast5Min() {
 
 		try {
 
 			SessionSearchPage sessionsearch_page = PageFactory.initElements(
 					driver, SessionSearchPage.class);
 			sessionsearch_page.sessSearch();
-			// sessionsearch_page.clickonSearchButton();
-			// sessionsearch_page.selectBBRsession();
-			// sessionsearch_page.backToSearch();
+
 			sessionsearch_page.clickonDefaultOption_Last24hrs();
 			sessionsearch_page.clickonOption_Last5min();
 			sessionsearch_page.clickonSearchButton();
+			sessionsearch_page.backToHome();
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -58,6 +52,23 @@ public class SessionSearch extends SaaSBasePage {
 
 	}
 
-	 
+	@Test(priority = 1)
+	public void sessionSearchDefault() {
+
+		try {
+
+			SessionSearchPage sessionsearch_page = PageFactory.initElements(
+					driver, SessionSearchPage.class);
+			sessionsearch_page.sessSearch();
+			sessionsearch_page.clickonSearchButton();
+			sessionsearch_page.selectBBRsession();
+			sessionsearch_page.backToSearch();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+
+	}
 
 }
