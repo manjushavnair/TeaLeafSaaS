0c03cbeaea92d685f053461c1e883f88974481ca
manjushasaju <manjushavnair@gmail.com>
2017-05-30 00:06:14 +0530

    as

---
+++ TealeafSaaS/src/com/ibm/tealeaf/testcases1/StruggleAnalyticsTest1.java
@@ -0,0 +1,65 @@
+/**
+ *  
+ */
+package com.ibm.tealeaf.testcases;
+
+/**
+ * @author Manjusha Saju
+ *
+ */
+//http://www.webtoolkitonline.com/xml-xpath-tester.html
+
+//	http://xmltoolbox.appspot.com/xpath_generator.html
+
+
+
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import com.ibm.tealeaf.commons.TeaLeafCONSTANTS;
+import com.ibm.tealeaf.pageobjects.StruggleAnalyticsPage;
+
+public class StruggleAnalyticsTest1 extends SaaSloginTest1 {
+
+	private static Logger logger = Logger.getLogger(StruggleAnalyticsTest1.class);
+
+	public StruggleAnalyticsTest1() {
+
+		 super();
+
+	}
+
+	@Test(priority = 2)
+	public void struggleAnalytics() {
+		try {
+
+			StruggleAnalyticsPage stru_page = PageFactory.initElements(driver,
+					StruggleAnalyticsPage.class);
+			
+			try {
+				Thread.sleep(TeaLeafCONSTANTS.WAITTIME30SEC);
+			} catch (Exception e) {
+				 
+				e.printStackTrace();
+			}
+			
+			stru_page.strugAnalytics();
+			stru_page.registration();
+			stru_page.backToStruggleAnalytics();
+			stru_page.shoppingCart();
+			stru_page.backToStruggleAnalytics();
+			stru_page.checkOut();
+			stru_page.backToStruggleAnalytics();
+			stru_page.bannerLandingPage();
+			stru_page.backToStruggleAnalytics();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+
+	}
+
+}
